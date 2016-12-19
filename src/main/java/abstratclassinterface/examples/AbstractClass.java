08c749e40888eb199952d03c39c562cce42d0dda
ramreddysrinivas <ramreddysrinivas532@gmail.com>
2016-12-19 09:29:53 +0530

    added exaples for abstract class and interface

---
+++ src/main/java/abstratclassinterface/examples/AbstractClass.java
@@ -0,0 +1,21 @@
+package abstratclassinterface.examples;
+
+public abstract class AbstractClass {
+
+	// Constructor name should be equal to Classname
+	// abstract class can contain constructor
+	public AbstractClass() {
+		System.out.println("AbstractSuper");
+	}
+
+	public abstract void abstractSuperM1();
+
+	public void abstractSuperM2() {
+		System.out.println("This is abstractSuperM2");
+	}
+
+	public void abstractSuperm3() {
+		// it is null body method
+	}
+
+}
---
+++ src/main/java/abstratclassinterface/examples/Interface1.java
@@ -0,0 +1,15 @@
+package abstratclassinterface.examples;
+
+public  interface Interface1 {
+	//public static final int C1 = 10;
+	int C1 = 10;
+	
+	//public abstract   void intefaceM1();
+	 void intefaceM1();
+	 /*void  intefaceM2() {
+		 }*/ // It will throw compilation error when we try to use Define method in Interface//
+	// public Interface1(){}
+	//Interface does not support Constructor concept & It throws Compilation Error when we try to Add Constructor
+		 
+	 
+}
