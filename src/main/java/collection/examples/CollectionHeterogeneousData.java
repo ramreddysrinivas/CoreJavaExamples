665688ab65904dffa4f927674613935a56272fb9
ramreddysrinivas <ramreddysrinivas532@gmail.com>
2017-01-23 09:07:25 +0530

    code added for collection

---
+++ src/main/java/collection/examples/CollectionHeterogeneousData.java
@@ -0,0 +1,18 @@
+package collection.examples;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionHeterogeneousData {
+	public static void main(String[] args) {
+		Collection collection = new ArrayList();
+		collection.add(5);
+		collection.add(true);
+		collection.add(5.5);
+		collection.add("4");
+		
+		collection.add('a');
+		//System.out.println(5+"4"+true );
+	}
+
+}
