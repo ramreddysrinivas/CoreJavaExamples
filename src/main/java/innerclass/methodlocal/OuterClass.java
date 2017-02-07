909170d523dc5736141becef7dced34ec6551987
ramreddysrinivas <ramreddysrinivas532@gmail.com>
2017-02-07 19:59:58 +0530

    code added for inner class methodloca

---
+++ src/main/java/innerclass/methodlocal/OuterClass.java
@@ -0,0 +1,23 @@
+package innerclass.methodlocal;
+
+public class OuterClass {
+	// instance method of the outer class 
+	   void my_Method() {
+	      final int num = 23;
+
+	      // method-local inner class
+	      class MethodInner_Demo {
+	         public void print() {
+	            System.out.println("This is method inner class "+num);	   
+	         }   
+	      } // end of inner class
+		   
+	      // Accessing the inner class
+	      MethodInner_Demo inner = new MethodInner_Demo();
+	      inner.print();
+	   }
+	   
+	   	   	   
+	   }
+
+
---
+++ src/main/java/innerclass/methodlocal/OuterDemo.java
@@ -0,0 +1,10 @@
+package innerclass.methodlocal;
+
+public class OuterDemo {
+	public static void main(String args[]) {
+	      OuterClass outer = new OuterClass();
+	      outer.my_Method();
+	 
+
+}
+}
